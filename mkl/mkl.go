3fd4d6b8e6fa587b168c3525d8bcb0239b3a3904
Tricky1975 <facebook@tbbs.nl>
2017-11-26 02:07:22 +0100

    Temporary set up list all

--- mkl/mkl.go
+++ mkl/mkl.go
@@ -9,3 +9,14 @@ func Version(n string,v string){
 func Lic(n string,l string){
     mkl_licenses[n] = l
 }
+
+func ListAll() string {
+   ret:=""
+   for k, v := range mkl_versions { 
+       //fmt.Printf("key[%s] value[%s]\n", k, v)
+       ret += k + " ... " + v + " "
+       ret += mkl_licenses[k]
+       ret += "\n"
+   }
+   return ret
+}
