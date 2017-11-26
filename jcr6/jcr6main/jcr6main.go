bafd35cf801fef7c84a2e2ed8aa018c061cb6018
Tricky1975 <facebook@tbbs.nl>
2017-11-26 22:34:06 +0100

    Skeleton driver

--- jcr6/jcr6main/jcr6main.go
+++ jcr6/jcr6main/jcr6main.go
@@ -1,20 +1,21 @@
 /*   -- Start License block
         jcr6main.go
 	(c) 2017 Jeroen Petrus Broks.
-
-	This Source Code Form is subject to the terms of the
-	Mozilla Public License, v. 2.0. If a copy of the MPL was not
-	distributed with this file, You can obtain one at
+	
+	This Source Code Form is subject to the terms of the 
+	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+	distributed with this file, You can obtain one at 
 	http://mozilla.org/MPL/2.0/.
         Version: 17.11.26
      -- End License block   */
+
 package jcr6main
 
 import (
 	"trickyunits/mkl"
 )
 
-type tJCR6Entry struct {
+type TJCR6Entry struct {
 	entry          string
 	mainfile       string
 	offset         int
@@ -33,13 +34,20 @@ type TJCR6Dir struct {
 }
 
 type TJCR6Driver struct {
+	drvname   string
 	recognize func(file string) bool
 	dir       func(file string) TJCR6Dir
 }
 
-var JCR6Driver = make(map[string]TJCR6Driver)
+var JCR6Drivers = make(map[string]*TJCR6Driver)
 
 func init() {
-	mkl.Version("Tricky's Go Units - jcr6main.go", "17.11.26")
-	mkl.Lic("Tricky's Go Units - jcr6main.go", "Mozilla Public License 2.0")
+mkl.Version("Tricky's Go Units - jcr6main.go","17.11.26")
+mkl.Lic    ("Tricky's Go Units - jcr6main.go","Mozilla Public License 2.0")
+	JCR6Drivers["JCR6"] = &TJCR6Driver{"JCR6", func(file string) bool {
+		return true
+	}, func(file string) TJCR6Dir {
+		ret := TJCR6Dir{make(map[string]TJCR6Entry), make(map[string]string)}
+		return ret
+	}}
 }
