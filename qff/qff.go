3c59869a07c721bdddbe9ebe86ed2c18304cadb6
Tricky1975 <facebook@tbbs.nl>
2017-11-27 01:37:06 +0100

    Fixed a few parse issues

--- qff/qff.go
+++ qff/qff.go
@@ -1,25 +1,28 @@
 /*   -- Start License block
-  qff.go
-  
-  version: 17.11.27
-  Copyright (C) 2017 Jeroen P. Broks
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-     -- End License block   */
+qff.go
+
+version: 17.11.27
+Copyright (C) 2017 Jeroen P. Broks
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+   -- End License block   */
 package qff
 
-import "trickyunits/mkl"
+import (
+	"os"
+	"trickyunits/mkl"
+)
 
 func Exists(name string) bool {
 	_, err := os.Stat(name)
@@ -27,6 +30,6 @@ func Exists(name string) bool {
 }
 
 func init() {
-mkl.Version("Tricky's Go Units - qff.go","17.11.27")
-mkl.Lic    ("Tricky's Go Units - qff.go","ZLib License")
+	mkl.Version("Tricky's Go Units - qff.go", "17.11.27")
+	mkl.Lic("Tricky's Go Units - qff.go", "ZLib License")
 }
---
+++ qff/readme.md
@@ -0,0 +1,5 @@
+# QFF
+
+QFF = Quick File Functions
+
+I've only set this up for a few quick functions I may need for file actions, that's all
