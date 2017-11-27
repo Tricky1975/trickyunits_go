2c15f55be8c48771fc77b210647a4aa2df686515
Tricky1975 <facebook@tbbs.nl>
2017-11-27 16:31:27 +0100

    Van alles en nog wat

--- jcr6/jcr6main/jcr6main.go
+++ jcr6/jcr6main/jcr6main.go
@@ -1,10 +1,20 @@
-/*   -- Start License block
+/*
         jcr6main.go
 	(c) 2017 Jeroen Petrus Broks.
 	
 	This Source Code Form is subject to the terms of the 
 	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
 	distributed with this file, You can obtain one at 
+	http://mozilla.org/MPL/2.0/.
+        Version: 17.11.27
+*/
+/*   -- Start License block
+        jcr6main.go
+	(c) 2017 Jeroen Petrus Broks.
+
+	This Source Code Form is subject to the terms of the
+	Mozilla Public License, v. 2.0. If a copy of the MPL was not
+	distributed with this file, You can obtain one at
 	http://mozilla.org/MPL/2.0/.
         Version: 17.11.27
      -- End License block   */
@@ -14,8 +24,10 @@ package jcr6main
 import (
 	//"io/ioutil"
 	"fmt"
+	"io"
 	"os"
 	"trickyunits/mkl"
+	"trickyunits/qerr"
 	"trickyunits/qff"
 )
 
@@ -41,8 +53,12 @@ type TJCR6Entry struct {
 }
 
 type TJCR6Dir struct {
-	entries  map[string]TJCR6Entry
-	comments map[string]string
+	entries   map[string]TJCR6Entry
+	comments  map[string]string
+	cfgint    map[string]int32
+	cfgbool   map[string]bool
+	cfgstr    map[string]string
+	fatoffset int32
 }
 
 type TJCR6Driver struct {
@@ -74,6 +90,19 @@ func Recognize(file string) string {
 	}
 	return ret
 }
+
+func Dir(file string) TJCR6Dir {
+	t := Recognize(file)
+	return JCR6Drivers[t].dir(file)
+}
+
+/*
+func JOpen(d TJCR6Dir, entry string) io.Reader {
+	chat("Opening: " + entry)
+
+}
+
+*/
 
 func init() {
 mkl.Version("Tricky's Go Units - jcr6main.go","17.11.27")
@@ -116,7 +145,44 @@ mkl.Lic    ("Tricky's Go Units - jcr6main.go","Mozilla Public License 2.0")
 		chat("All is fine for the JCR6 type")
 		return true
 	}, func(file string) TJCR6Dir {
-		ret := TJCR6Dir{make(map[string]TJCR6Entry), make(map[string]string)}
+		JCR6Error = ""
+		ret := TJCR6Dir{make(map[string]TJCR6Entry), make(map[string]string), make(map[string]int32), make(map[string]bool), make(map[string]string), 0}
+		bt, e := os.Open(file)
+		qerr.QERR(e)
+		if qff.RawReadString(bt, 5) != "JCR6\x1a" {
+			panic("YIKES!!! A NONE JCR6 FILE!!!! HELP! HELP! I'M DYING!!!")
+		}
+		ret.fatoffset = qff.ReadInt32(bt)
+		if ret.fatoffset <= 0 {
+			JCR6Error = "Invalid FAT offset. Maybe you are trying to read a JCR6 file that has never been properly finalized"
+			bt.Close()
+			return ret
+		}
+		var TTag byte
+		var Tag string
+		TTag = qff.ReadByte(bt)
+		for TTag != 255 {
+			Tag = qff.ReadString(bt)
+			switch TTag {
+			case 1:
+				ret.cfgstr[Tag] = qff.ReadString(bt)
+			case 2:
+				ret.cfgbool[Tag] = qff.ReadByte(bt) == 1
+			case 3:
+				ret.cfgint[Tag] = qff.ReadInt32(bt)
+			default:
+				JCR6Error = "Invalid config tag"
+				bt.Close()
+				return ret
+			}
+		}
+		if ret.cfgbool["_CaseSensitive"] {
+			JCR6Error = "Case Sensitive dir support was already deprecated and removed from JCR6 before it went to the Go language. It's only obvious that support for this was never implemented in Go in the first place."
+			bt.Close()
+			return ret
+		}
+
+		bt.Close()
 		return ret
 	}}
 
