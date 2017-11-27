9a47a5860482aad7c079fc17d754a9ae0426a7c0
Tricky1975 <facebook@tbbs.nl>
2017-11-27 16:30:53 +0100

    Lic

--- qstr/qstr.go
+++ qstr/qstr.go
@@ -1,3 +1,23 @@
+/*
+  qstr.go
+  
+  version: 17.11.27
+  Copyright (C) 2017 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+package qstr
 
 // This function was set up by PeterSO on StackOverflow
 // https://stackoverflow.com/questions/14230145/what-is-the-best-way-to-convert-byte-array-to-string
@@ -16,4 +36,3 @@ func CToGoString(c []byte) string {
 func BA2S(c []byte) string {
 	return CToGoString(c[:])
 }
-   
