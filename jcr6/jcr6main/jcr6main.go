1b8c63a93838a67860c24bea5a7ede6c15769afa
Tricky1975 <facebook@tbbs.nl>
2017-11-28 10:48:52 +0100

    All danger gone now?

--- jcr6/jcr6main/jcr6main.go
+++ jcr6/jcr6main/jcr6main.go
@@ -7,16 +7,6 @@
 	distributed with this file, You can obtain one at 
 	http://mozilla.org/MPL/2.0/.
         Version: 17.11.28
-*/ /*   -- Start License block
-        jcr6main.go
-	(c) 2017 Jeroen Petrus Broks.
-
-	This Source Code Form is subject to the terms of the
-	Mozilla Public License, v. 2.0. If a copy of the MPL was not
-	distributed with this file, You can obtain one at
-	http://mozilla.org/MPL/2.0/.
-        Version: 17.11.27
-     -- End License block   
 */
 
 package jcr6main
