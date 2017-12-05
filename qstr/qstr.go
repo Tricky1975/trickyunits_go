8b25269033342adba30e1acb533fa2e824abf0f7
Tricky1975 <facebook@tbbs.nl>
2017-12-05 11:55:16 +0100

    MyTrim

--- qstr/qstr.go
+++ qstr/qstr.go
@@ -1,7 +1,7 @@
 /*
   qstr.go
   
-  version: 17.12.04
+  version: 17.12.05
   Copyright (C) 2017 Jeroen P. Broks
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -143,5 +143,5 @@ func MyTrim(a string) string{
 
 func init(){
 mkl.Lic    ("Tricky's Go Units - qstr.go","ZLib License")
-mkl.Version("Tricky's Go Units - qstr.go","17.12.04")
+mkl.Version("Tricky's Go Units - qstr.go","17.12.05")
 }
