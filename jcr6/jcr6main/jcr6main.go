739d1fd93f992187422793cd41cc1e6ccdae3198
Tricky1975 <facebook@tbbs.nl>
2017-12-22 18:49:54 +0100

    Parse error fixes

--- jcr6/jcr6main/jcr6main.go
+++ jcr6/jcr6main/jcr6main.go
@@ -242,24 +242,6 @@ func JCR_String(j TJCR6Dir,entry string) string {
 }
 
 
-func JCR_Lines(jcr TJCR6Dir,filename string) []string {
-	s:=GetString(filename)
-	// The two character line break MUST be checked first, or else the
-	// one character types will dominate everything causing faulty
-	// results.
-	if strings.Index(s,"\r\n")>=0 {
-		return strings.Split(s,"\r\n")
-	} else if strings.Index(s,"\n\r")>=0 {
-		return strings.Split(s,"\r\n")
-	} else if strings.Index(s,"\n")>=0 {
-		return strings.Split(s,"\n")
-	} else if strings.Index(s,"\r")>=0 {
-		return strings.Split(s,"\r")
-	} else {
-		return []string{s}
-	}
-}
-
 
 // Extracts a file
 // If the unix permissions are known these will be set automatically
@@ -281,7 +263,7 @@ func JCR_Extract(j TJCR6Dir,entry,extractto string) {
 // Gives the content of a text files line by line.
 func JCR_ListEntry(j TJCR6Dir,entry string) []string {
 	//r:=strings.Split(JCR_String(j,entry),"\n")
-	s:=JCR_String(j,filename)
+	s:=JCR_String(j,entry)
 	// The two character line break MUST be checked first, or else the
 	// one character types will dominate everything causing faulty
 	// results.
@@ -298,8 +280,6 @@ func JCR_ListEntry(j TJCR6Dir,entry string) []string {
 	}
 }
 
-	return r
-}
 
 func JCR6_JamErr(AError string,AFile string,AEntry string,AFunc string) {
 	e:="**** JCR 6 ERROR ****\n"
